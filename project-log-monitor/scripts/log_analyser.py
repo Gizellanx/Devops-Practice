0dad96253feab8f096839d694ea238a34c89cb32
Gizellanx <gizellan10@gmail.com>
2026-04-16 12:10:07 +0100

    Update log analyser to save output report

--- project-log-monitor/scripts/log_analyser.py
+++ project-log-monitor/scripts/log_analyser.py
@@ -1,6 +1,7 @@
 from collections import Counter
 
 log_file = "../logs/access.log"
+output_file = "../output/report.txt"
 
 ip_addresses = []
 
@@ -14,3 +15,9 @@ most_common_ip, count = counter.most_common(1)[0]
 
 print(f"Most active IP: {most_common_ip}")
 print(f"Number of requests: {count}")
+
+with open(output_file, "w") as file:
+    file.write(f"Most active IP: {most_common_ip}\n")
+    file.write(f"Number of requests: {count}\n")
+
+print("Report saved successfully.")
