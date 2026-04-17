352445e6beb4729e71ccee0868a4768bdf58017b
Gizellanx <gizellan10@gmail.com>
2026-04-17 14:21:40 +0100

    Add suspicious IP detection script using failed request filtering

---
+++ project-log-monitor/scripts/log_analyser_suspicious.py
@@ -0,0 +1,19 @@
+from collections import Counter
+
+log_file = "../logs/access.log"
+
+failed_ips = []
+
+with open(log_file, "r") as file:
+    for line in file:
+        ip = line.split()[0]
+        status_code = line.split()[-1]
+
+        if status_code.startswith("4") or status_code.startswith("5"):
+            failed_ips.append(ip)
+
+counter = Counter(failed_ips)
+
+for ip, count in counter.items():
+    if count > 5:
+        print(f"{ip} → {count} failed requests")
